484cd7f1d9506b770ecc358c7bb970fc3dc33b2e
Daniel Mendez <daniel.mendeza@wom.co>
2025-03-01 12:14:19 -0500

    v 0.0.3

--- programa.js
+++ programa.js
@@ -4,3 +4,28 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+async function loadPolygon() {
+
+    try{
+
+        let response=await fetch("mazuren.geojson");
+        let data = await response.json();
+
+        L.geoJSON (data,
+        {
+
+            style:{color: "blue"}
+        }
+        ).addTo(map);
+ 
+    }
+    catch(error){
+
+        console.error("can't load")
+    }
+    
+}
+
+loadPolygon();
